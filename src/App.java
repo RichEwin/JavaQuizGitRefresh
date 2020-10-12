42fd800f867695f50f8bebd7219cdfd4cd240fad
RichEwin <richardewinlambert@gmail.com>
2020-10-12 18:27:46 +0100

    additional question created + testing out rebasing

--- src/App.java
+++ src/App.java
@@ -10,21 +10,26 @@ public class App {
         String q2 = "What color is a banana?\n"
                     + "(a)orange\n(b)yellow\n(c)brown";
 
+         String q3 = "What color is a apple?\n"
+                    + "(a)orange\n(b)red\n(c)black";
+
+
         Question [] questions = {
                 new Question(q1, "a"),
                 new Question(q2, "b")
+                new Question(q3, "a")
         };
     }
 
-    public static void takeTest(Question [] questions) {
+    public static void takeTest(Question[] questions) {
         int score = 0;
 
         Scanner input = new Scanner(System.in);
 
-        for(int i = 0; i < questions.length; i++) {
+        for (int i = 0; i < questions.length; i++) {
             System.out.println(questions[i].prompt);
             String answer = input.nextLine();
-            if(answer.equals(questions[i].answer)) {
+            if (answer.equals(questions[i].answer)) {
                 score++;
             }
         }
